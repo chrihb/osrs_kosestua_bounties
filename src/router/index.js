9c79e8ea7c98caff3c1b05cba71ea638f01e3e58
chrihb <chrihb@stud.ntnu.no>
2026-03-14 23:45:52 +0100

    gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import LoginView from "@/views/LoginView.vue";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/osrs_kosestua_bounties/'),
     routes: [
         {
             path: "/",
