68844a738f8205d4b4d528264b7d93a929b8b13b
chrihb <chrihb@stud.ntnu.no>
2026-03-15 00:05:35 +0100

    gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "@/stores/authStore.js";
 import LoginView from "@/views/loginView.vue";
 import HomeView from "@/views/homeView.vue";
+import App from "@/App.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
